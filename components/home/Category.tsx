51cda821ccfcc0acfecf6b92bf36826127c5094c
yafet-123 <yafetaddisu123@gmail.com>
2023-03-28 12:32:30 -0700

    editing category of the pages index.js

--- components/home/Category.tsx
+++ components/home/Category.tsx
@@ -3,10 +3,10 @@ import { CategoryData }  from '../../data/Category'
 
 export const Category: React.FC<any> = () => {
   return (
-    <div className="flex h-full lg:px-20" >
-      <ul className="flex h-[45rem] flex-col justify-center items-center flex-wrap gap-8 font-bold break-words">
+    <div className="flex justify-center items-center h-full w-full lg:px-20" >
+      <ul className="flex w-full h-[38rem] lg:h-[45rem] flex-col flex-wrap gap-8 font-bold">
         { CategoryData?.map((category:any, index:any)=>(
-          <li key={index} className="text-lg lg:text-2xl font-bold text-[#6471DD] dark:text-[#C1B0EB] lg:mb-2">{category.title}</li>
+          <li key={index} className="text-center text-lg lg:text-2xl font-bold text-[#6471DD] dark:text-[#C1B0EB] lg:mb-2">{category.title}</li>
         ))}
       </ul>
     </div>
