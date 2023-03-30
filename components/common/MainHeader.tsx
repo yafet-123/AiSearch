5966d74a8b7320f249b542340a9d23339b4195bc
yafet-123 <yafetaddisu123@gmail.com>
2023-03-30 14:43:17 -0700

    adding nextauth pages

--- components/common/MainHeader.tsx
+++ components/common/MainHeader.tsx
@@ -14,17 +14,7 @@ export const MainHeader: React.FC<HeaderTitle> = ({ title }) => {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <link rel="manifest" href="public/manifest.json" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
+        
         <link
           rel="icon"
           type="image/png"
