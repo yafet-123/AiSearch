a16432b7354f466515ef1e7210e0a5c3c7925fe2
yafet-123 <yafetaddisu123@gmail.com>
2023-05-16 11:50:01 -0700

    adding the about and team page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
 export default function HomePage(){
 	return(
 		<div>
-			<h1 className="text-4xl">Welcome to the Blog</h1>
-			<p>Some content</p>
+			<h1>Welcome To Traversy Meida</h1>
 		</div>
 	)
 }
